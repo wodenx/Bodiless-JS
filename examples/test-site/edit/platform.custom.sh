839f931f594742f635bc9f0b7755375cb5383008
Chris Oden <coden@its.jnj.com>
2020-01-21 14:02:58 -0500

    Ensure docs are build in psh.

--- examples/test-site/edit/platform.custom.sh
+++ examples/test-site/edit/platform.custom.sh
@@ -22,6 +22,7 @@ if [ "$1" = "install" ]; then
   npm run bootstrap -- --ci
 elif [ "$1" = "build" ]; then
   npm run build:packages -- --concurrency 1
+  npx lerna run build:doc --scope @bodiless/test-site
 elif [ "$1" = "finish-deploy" ]; then
   SITE_DIR=${ROOT_DIR}/examples/test-site
   cp ${PLATFORM_APP_DIR}/${DEFAULT_ENV} ${SITE_DIR}/.env.development
