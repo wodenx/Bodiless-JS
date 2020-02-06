f01f5ac67cfd61af24b57383a930054100c35b20
Chris Oden <coden@its.jnj.com>
2020-02-06 14:05:13 -0500

    Update path prefix.

--- packages/bodiless-documentation/src/createBar.ts
+++ packages/bodiless-documentation/src/createBar.ts
@@ -34,7 +34,7 @@ const createBar = (
   writeHeader: boolean = true,
   indent: number = 1,
 ) => {
-  const pathPrefix = parent.map((item:string) => `${item}/`).join('');
+  const pathPrefix = `/${parent.map((item:string) => `${item}/`).join('')}`;
   const linePrefix = ' '.repeat(indent * 2);
   const header = writeHeader && parent.length > 0 && parent.slice(-1)[0];
   let navbar = header ? `* ${niceName(header.replace(/.md$/, ''))}\n` : '';
@@ -46,7 +46,7 @@ const createBar = (
       const branch = paths[key] as Tree;
       const subLines = depth > 0 || !branch['README.md'] ? createBar(paths[key] as Tree, [...parent, key], depth - 1, false, indent + 1) : '';
       if (branch['README.md']) {
-        line = `${linePrefix}* [${name}](/${pathPrefix}${link}/)\n`;
+        line = `${linePrefix}* [${name}](${pathPrefix}${link}/)\n`;
         // If we have subLines but we have no README then do not add the tree item
       } else if (subLines) {
         line = `${linePrefix}* ${name}\n`;
