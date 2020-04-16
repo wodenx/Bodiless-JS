f2af0f65aab39523b3aa38d2893284d3b8da78e8
Chris Oden <coden@its.jnj.com>
2020-04-16 08:29:18 -0400

    Add rough test case

--- packages/bodiless-components/src/filterHOC.tsx
+++ packages/bodiless-components/src/filterHOC.tsx
@@ -1,8 +1,8 @@
-import React, { ComponentType, Fragment } from 'react';
+import React, { ComponentType, Fragment, useState } from 'react';
 import { observer } from 'mobx-react-lite';
-import { useNode } from '@bodiless/core';
+import { useNode, withNode } from '@bodiless/core';
 import { replaceWith } from '@bodiless/fclasses';
-import { difference, flowRight } from 'lodash';
+import { difference, flowRight, flow } from 'lodash';
 
 type ToggleHook = (props: any) => boolean;
 
@@ -53,3 +53,36 @@ const withFilterByTags = ifTagsSelected(
 );
 
 export { ifTagsSelected, withFilterByTags, ifTagsNotSelected };
+
+
+// For each of these we'll have to create a mock node (or json file)
+// which provides the correct item tag
+const FilterableItem = flow(
+  withFilterByTags,
+  withNode,
+)('div');
+
+export const TestFilterSelector = () => {
+  const [tags, setTags] = useState<string[]>([]);
+  return (
+    <div>
+      <div>
+        <h2>Select a tag to filter by</h2>
+        <a onClick={() => setTags(['foo'])}>foo</a>
+        <a onClick={() => setTags(['bar'])}>bar</a>
+        <a onClick={() => setTags(['baz'])}>baz</a>
+        <a onClick={() => setTags(['bat'])}>bat</a>
+      </div>
+      <div>
+        <h2>Filtered Components</h2>
+        <FilterableItem nodeKey="foo" selectedTags={tags}>foo</FilterableItem>
+        <FilterableItem nodeKey="bar" selectedTags={tags}>bar</FilterableItem>
+        <FilterableItem nodeKey="baz" selectedTags={tags}>baz</FilterableItem>
+        <FilterableItem nodeKey="bat" selectedTags={tags}>bat</FilterableItem>
+      </div>
+    </div>
+  )
+
+}
+
+
