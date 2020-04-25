e4b425a2a108eaea05bc69cd2a85acb83668ebcf
Chris Oden <coden@its.jnj.com>
2020-04-25 07:37:32 -0400

    Fix filter difference algorithm and type error.

--- packages/bodiless-components/src/filterHOC.tsx
+++ packages/bodiless-components/src/filterHOC.tsx
@@ -2,9 +2,10 @@ import {
   withoutProps, ifToggledOn, ifToggledOff, TagType,
 } from '@bodiless/core';
 import { replaceWith } from '@bodiless/fclasses';
-import { difference, flowRight } from 'lodash';
+import { difference, flowRight, differenceWith } from 'lodash';
 import { toJS } from 'mobx';
 import useTagsAccessors from './TagButton/TagModel';
+import { ComponentType } from 'react';
 
 type ToggleByTagsProps = {
   selectedTags: TagType[];
@@ -24,15 +25,15 @@ const useToggleByTags = <P extends object>({
   console.log('item tags', toJS(tags));
   console.log('differnece', difference(selectedTags, tags));
   // const tags = itemTags.map(t => t.name);
-  return difference(selectedTags, tags).length === 0;
+  return differenceWith(selectedTags, tags, (selectedTag, itemTag) => selectedTag.name === itemTag.name).length === 0;
 };
 
 const ifTagsSelected = ifToggledOn(useToggleByTags);
 const ifTagsNotSelected = ifToggledOff(useToggleByTags);
 
-const withFilterByTags = flowRight(
+const withFilterByTags = <P extends object>(C: ComponentType<P>) => flowRight(
   ifTagsNotSelected(replaceWith(() => null)),
   withoutProps(['selectedTags']),
-);
+)(C) as ComponentType<P & ToggleByTagsProps>;
 
 export { ifTagsSelected, withFilterByTags, ifTagsNotSelected };
