b967a395b725ee48fbd4cf120efa1a6f3a5dd10a
Chris Oden <coden@its.jnj.com>
2020-05-13 07:18:47 -0400

    Improved tests, component display names and nested sidecars.

--- packages/bodiless-core/__tests__/withSidecarNodes.test.tsx
+++ packages/bodiless-core/__tests__/withSidecarNodes.test.tsx
@@ -1,14 +1,20 @@
 import React, { ComponentType } from 'react';
 import { flowRight } from 'lodash';
-import { mount, render } from 'enzyme';
+import { mount } from 'enzyme';
 import withNode, { withNodeKey } from '../src/withNode';
 import { useNode } from '../src/NodeProvider';
-import withSidecarNodes from '../src/withSidecarNodes';
+import withSidecarNodes, { endSidecarNodes } from '../src/withSidecarNodes';
+import { ifToggledOn } from '../src/withFlowToggle';
 
 type HOC = (C: ComponentType<any>) => ComponentType<any>;
 type Bodiless = (key?: string) => HOC;
 
-const withEnhancement = (id: string) => (Component: ComponentType<any>) => withNode(
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const withProps: (propsToAdd: any) => HOC = propsToAdd => Component => props => (
+  <Component {...propsToAdd} {...props} />
+);
+
+const withPropEnhancement = (id: string) => (Component: ComponentType<any>) => (
   (props: any) => {
     const { node } = useNode();
     const enhancement = {
@@ -17,121 +23,201 @@ const withEnhancement = (id: string) => (Component: ComponentType<any>) => withN
     return (
       <Component {...props} {...enhancement} />
     );
-  },
-);
-
-// const withId: (id: string) => HOC = id => Component => props => (
-//   <Component {...props} id={id} />
-// );
-
-const NodePathPrinter = withNode(
-  (props: any) => (<span {...props}>{useNode().node.path.join('$')}</span>),
-);
-
-const CompoundPrinter = withNode(
-  ({ printer: Printer, ...rest }: any) => (
-    <span {...rest}>
-      <Printer id="baz" />
-    </span>
-  ),
+  }
 );
 
 const withFoo: Bodiless = (nodeKey?: string) => flowRight(
   withNodeKey(nodeKey),
-  withEnhancement('foo'),
+  withNode,
+  withPropEnhancement('foo'),
 );
 
 const withBar: Bodiless = (nodeKey?: string) => flowRight(
   withNodeKey(nodeKey),
-  withEnhancement('bar'),
+  withNode,
+  withPropEnhancement('bar'),
 );
 
+const withBaz: Bodiless = (nodeKey?: string) => flowRight(
+  withNodeKey(nodeKey),
+  withNode,
+  withPropEnhancement('baz'),
+);
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const withWrapperEnhancement = (id: string) => (Component: ComponentType<any>) => (
+  (props: any) => {
+    const { node } = useNode();
+    const enhancement = {
+      [`data-enh-${id}`]: node.path.join('$'),
+    };
+    return (
+      <span {...enhancement} id={`enh-${id}`}>
+        <Component {...props} />
+      </span>
+    );
+  }
+);
+
+const spanWithId = (id: string) => (props: any) => <span {...props} id={id} />;
+
+describe('Sidecar Node Use Cases', () => {
+  describe('Adding a toggle', () => {
+    const mockNodeData = jest.fn();
+    const useDataToggle = () => mockNodeData(useNode<any>().node.path.join('$'));
+    const withToggleTo = (ToComponent: ComponentType<any>) => (nodeKey?: string) => flowRight(
+      withNodeKey(nodeKey),
+      withNode,
+      ifToggledOn(useDataToggle)(
+        () => ToComponent,
+      ),
+    );
+    const Span = spanWithId('test');
+    it('has the correct baseline', () => {
+      const Test = withFoo('foo')(Span);
+      const wrapper = mount(<Test />);
+      expect(wrapper.find('span#test').prop('data-enh-foo')).toBe('root$foo');
+    });
+    describe('Without sidecar', () => {
+      const Bar = withBar('foo')(spanWithId('bar'));
+      const Test = flowRight(
+        withToggleTo(Bar)('toggle'),
+        withFoo('foo'),
+      )(Span);
+      it('modifies the child nodekey when toggled off', () => {
+        mockNodeData.mockReset();
+        mockNodeData.mockReturnValueOnce(false);
+        const wrapper = mount(<Test />);
+        expect(mockNodeData.mock.calls[0][0]).toBe('root$toggle');
+        expect(wrapper.find('span#bar')).toHaveLength(0);
+        expect(wrapper.find('span#test').prop('data-enh-foo')).toBe('root$toggle$foo');
+      });
+      it('modifies the child nodekey when toggled on', () => {
+        mockNodeData.mockReset();
+        mockNodeData.mockReturnValueOnce(true);
+        const wrapper = mount(<Test />);
+        expect(mockNodeData.mock.calls[0][0]).toBe('root$toggle');
+        expect(wrapper.find('span#test')).toHaveLength(0);
+        expect(wrapper.find('span#bar').prop('data-enh-bar')).toBe('root$toggle$foo');
+      });
+    });
+    describe('With sidecar', () => {
+      const Bar = withBar('foo')(spanWithId('bar'));
+      const Test = flowRight(
+        withSidecarNodes(
+          withToggleTo(endSidecarNodes(Bar))('toggle'),
+        ),
+        withFoo('foo'),
+      )(Span);
+      it('does not change the child nodekey when toggled off', () => {
+        mockNodeData.mockReset();
+        mockNodeData.mockReturnValueOnce(false);
+        const wrapper = mount(<Test />);
+        expect(mockNodeData.mock.calls[0][0]).toBe('root$toggle');
+        expect(wrapper.find('span#bar')).toHaveLength(0);
+        expect(wrapper.find('span#test').prop('data-enh-foo')).toBe('root$foo');
+      });
+      it('does not modify the child nodekey when toggled on', () => {
+        mockNodeData.mockReset();
+        mockNodeData.mockReturnValueOnce(true);
+        const wrapper = mount(<Test />);
+        expect(mockNodeData.mock.calls[0][0]).toBe('root$toggle');
+        expect(wrapper.find('span#test')).toHaveLength(0);
+        expect(wrapper.find('span#bar').prop('data-enh-bar')).toBe('root$foo');
+      });
+    });
+  });
+  describe('Enhancing a flow container item wrapper without changing data', () => {
+    const ChildNodeProvider:any = withNode(React.Fragment);
+    const Item:any = withFoo()(spanWithId('item'));
+    const BasicWrapper:any = spanWithId('wrapper');
+    const asFlowContainerItem = (Wrapper: any) => (
+      <ChildNodeProvider nodeKey="child">
+        <Wrapper>
+          <Item />
+        </Wrapper>
+      </ChildNodeProvider>
+    );
+    it('has a correct baseline', () => {
+      const baseline = asFlowContainerItem(BasicWrapper);
+      const wrapper = mount(baseline);
+      expect(wrapper.find('span#item').prop('data-enh-foo')).toBe('root$child');
+    });
+    it('leaves original nodekey unchanged when enhancement is in sidecar', () => {
+      const EnhancedWrapper = withSidecarNodes(withBar('barkey'))(BasicWrapper);
+      const test = asFlowContainerItem(EnhancedWrapper);
+      const wrapper = mount(test);
+      expect(wrapper.find('span#item').prop('data-enh-foo')).toBe('root$child');
+      expect(wrapper.find('span#wrapper').prop('data-enh-bar')).toBe('root$child$barkey');
+    });
+    it('changes the original nodekey when enhancement is not in sidecar', () => {
+      const EnhancedWrapper = withBar('barkey')(BasicWrapper);
+      const test = asFlowContainerItem(EnhancedWrapper);
+      const wrapper = mount(test);
+      expect(wrapper.find('span#item').prop('data-enh-foo')).toBe('root$child$barkey');
+      expect(wrapper.find('span#wrapper').prop('data-enh-bar')).toBe('root$child$barkey');
+    });
+  });
+});
 
 describe('withSidecarNodes', () => {
-  it('is tested against the correct baseline', () => {
-    const asTestWithoutEnhancement: HOC = flowRight(
-      withNodeKey('test'),
-    );
-    const Test = asTestWithoutEnhancement(NodePathPrinter);
-    const wrapper = mount(<Test id="test" />);
-    expect(wrapper.find('span#test').text()).toBe('root$test');
-  });
-
-  it('adds new children and restores original node', () => {
-    const asTestWithEnhancement: HOC = flowRight(
-      withNodeKey('test'),
-      withSidecarNodes(
-        withFoo('foo'),
-        withBar('bar'),
-      ),
-    );
-    const Test = asTestWithEnhancement(NodePathPrinter);
-    const wrapper = mount(<Test id="test" />);
-    const span = wrapper.find('span#test');
-    expect(span.text()).toBe('root$test');
-    expect(span.prop('data-enh-foo')).toBe('root$test$foo');
-    expect(span.prop('data-enh-bar')).toBe('root$test$foo$bar');
-  });
-
-
-  it('can create multiple sidecars', () => {
-    const asTest: HOC = flowRight(
-      withNodeKey('test'),
+  it('Preserves the original nodeKey when an HOC is applied outside.', () => {
+    const Test = flowRight(
       withSidecarNodes(
         withFoo('foo'),
       ),
-      withSidecarNodes(
-        withBar('bar'),
-      ),
-    );
-    const Test = asTest(NodePathPrinter);
-    const wrapper = mount(<Test id="test" />);
-    const span = wrapper.find('span#test');
-    expect(span.text()).toBe('root$test');
-    expect(span.prop('data-enh-foo')).toBe('root$test$foo');
-    expect(span.prop('data-enh-bar')).toBe('root$test$bar');
+      withBar('bar'),
+    )(spanWithId('test'));
+    const wrapper = mount(<Test />);
+    expect(wrapper.find('span#test').prop('data-enh-foo')).toBe('root$foo');
+    expect(wrapper.find('span#test').prop('data-enh-bar')).toBe('root$bar');
   });
-
-  const nestedBaseline: { Printer: ComponentType<any>, Compound: ComponentType<any> } = {
-    Printer: withNodeKey('baz')(NodePathPrinter),
-    Compound: withNodeKey('test')(CompoundPrinter),
-  };
-
-  const mountNested = ({ Printer, Compound }: any) => render(<Compound printer={Printer} id="test" />);
-
-  it('is tested against the correct baseline for nested sidecars', () => {
-    const wrapper = mountNested(nestedBaseline);
-    const span = wrapper.find('span#baz');
-    expect(span.text()).toBe('root$test$baz');
-  });
-
-
-  it.only('supports nested sidecars', () => {
-    const { Printer: BaselinePrinter, Compound: BaselineCompound } = nestedBaseline;
-    const Compound = withSidecarNodes(withFoo('foo'))(BaselineCompound);
-    const Printer = withSidecarNodes(withBar('bar'))(BaselinePrinter);
-    const wrapper = mountNested({ Printer, Compound });
-    console.log(wrapper.parent().html());
-    const innerSpan = wrapper.find('span#baz');
-    expect(innerSpan.text()).toBe('root$test$baz');
-    expect(innerSpan.prop('data-enh-bar')).toBe('root$test$baz$bar');
-    const outerSpan = wrapper.find('span#test');
-    expect(outerSpan.prop('data-enh-foo')).toBe('root$test$foo');
-  });
-
-  it('adds new peers and restores original node', () => {
-    const asTestWithEnhancement: HOC = flowRight(
+  it('Preserves the original nodeKey when wrapping a component.', () => {
+    const Bar = withBar('bar')(spanWithId('test'));
+    const baseline = mount(<Bar />);
+    const Test = flowRight(
       withSidecarNodes(
         withFoo('foo'),
+      ),
+    )(Bar);
+    const wrapper = mount(<Test />);
+    expect(wrapper.find('span#test').prop('data-enh-foo')).toBe('root$foo');
+    expect(wrapper.find('span#test').prop('data-enh-bar')).toBe('root$bar');
+    expect(baseline.find('span#test').prop('data-enh-bar')).toBe('root$bar');
+  });
+  it('Supports multiple sidecars', () => {
+    const Test = flowRight(
+      withFoo('foo'),
+      withSidecarNodes(
         withBar('bar'),
       ),
-      withNodeKey('test'),
-    );
-    const Test = asTestWithEnhancement(NodePathPrinter);
-    const wrapper = mount(<Test id="test" />);
+      withSidecarNodes(
+        withBaz('baz'),
+      ),
+    )(spanWithId('test'));
+    const wrapper = mount(<Test />);
+    expect(wrapper.find('span#test').prop('data-enh-foo')).toBe('root$foo');
+    expect(wrapper.find('span#test').prop('data-enh-bar')).toBe('root$foo$bar');
+    expect(wrapper.find('span#test').prop('data-enh-baz')).toBe('root$foo$baz');
+  });
+  it('Supports nested sidecars', () => {
+    const Test = flowRight(
+      withSidecarNodes(
+        withNodeKey('foobar'),
+        withNode,
+        withSidecarNodes(
+          withSidecarNodes(
+            withBar('bar'),
+          ),
+          withFoo('foo'),
+        ),
+      ),
+      withBaz('baz'),
+    )(spanWithId('test'));
+    const wrapper = mount(<Test />);
     const span = wrapper.find('span#test');
-    expect(span.text()).toBe('root$test');
-    expect(span.prop('data-enh-foo')).toBe('root$foo');
-    expect(span.prop('data-enh-bar')).toBe('root$foo$bar');
+    expect(span.prop('data-enh-foo')).toBe('root$foobar$foo');
+    expect(span.prop('data-enh-bar')).toBe('root$foobar$bar');
+    expect(span.prop('data-enh-baz')).toBe('root$baz');
   });
 });
--- packages/bodiless-core/src/withSidecarNodes.tsx
+++ packages/bodiless-core/src/withSidecarNodes.tsx
@@ -2,30 +2,90 @@
 import React, { createContext, useContext, ComponentType } from 'react';
 import { flowRight } from 'lodash';
 import NodeProvider, { useNode } from './NodeProvider';
-import { ContentNode, DefaultContentNode } from './ContentNode';
-import withNode from './withNode';
+import { ContentNode } from './ContentNode';
 
-const SidecarNodeContext = createContext<ContentNode<any>>(DefaultContentNode.dummy());
+const SidecarNodeContext = createContext<ContentNode<any>[]>([]);
 
-const startSidecarNodes = <P extends object>(Component: ComponentType<P>) => (props: P) => (
-  <SidecarNodeContext.Provider value={useNode().node}>
-    <Component {...props} />
-  </SidecarNodeContext.Provider>
-);
+/**
+ * `startSidecarNodes` is an HOC which records the current ContentNode so that
+ * it can later be restored.
+ *
+ * @see `withSidecarNodes`
+ *
+ * @param Component Any component which uses the Bodiless ContentNode system.
+ */
+const startSidecarNodes = <P extends object>(Component: ComponentType<P>) => {
+  const StartSidecarNodes = (props: P) => {
+    const oldValue = useContext(SidecarNodeContext);
+    const newValue = [...oldValue, useNode().node];
+    return (
+      <SidecarNodeContext.Provider value={newValue}>
+        <Component {...props} />
+      </SidecarNodeContext.Provider>
+    );
+  };
+  StartSidecarNodes.displayName = 'StartSidecarNodes';
+  return StartSidecarNodes;
+};
 
-const endSidecarNodes = <P extends object>(Component: ComponentType<P>) => (props: P) => (
-  <NodeProvider node={useContext(SidecarNodeContext)}>
-    <Component {...props} />
-  </NodeProvider>
-);
+
+/**
+ * `endSidecarNodes` is an HOC which restores the ContentNode preserved
+ * by `startSidecarNodes`.
+ *
+ * @see `withSidecarNodes`
+ *
+ * @param Component Any component which uses the Bodiless ContentNode system.
+ */
+const endSidecarNodes = <P extends object>(Component: ComponentType<P>) => {
+  const EndSidecarNodes = (props: P) => {
+    const oldValue = useContext(SidecarNodeContext);
+    if (oldValue.length === 0) return <Component {...props} />;
+    const newNode = oldValue[oldValue.length - 1];
+    const newValue = oldValue.slice(0, -1);
+    return (
+      <NodeProvider node={newNode}>
+        <SidecarNodeContext.Provider value={newValue}>
+          <Component {...props} />
+        </SidecarNodeContext.Provider>
+      </NodeProvider>
+    );
+  };
+  EndSidecarNodes.displayName = 'EndSidecarNodes';
+  return EndSidecarNodes;
+};
 
 type HOC = (Component: ComponentType<any>) => ComponentType<any>;
 
+/**
+ * `withSidecarNodes` allows you to establish a `ContentNode` sub-hierarchiy which should
+ * be used by a series of one or more HOC's. Any nodes created by the HOC's enclosed in this
+ * wrapper will not affect the hierarchy for subsequent HOC's *outside* the wrapper. For
+ * example:
+ * ```js
+ * flowRight(
+ *   ...
+ *   withNodeKey('foo'), withNode,  // ...$foo
+ *   withSidecarNodes(
+ *     withNodeKey('bar'), withNode,  // ...$foo$bar
+ *   ),
+ *   withNodeKey('baz'); withNode, // ...$foo$baz (otherwise would be ...$foo$bar$baz)
+ *   ...
+ * )
+ * ```
+ * This is useful, for example, if you want to apply an enhancment HOC which uses its own
+ * content node(s) without affecting the node paths of other children of the wrapped compoenent.
+ *
+ * @param hocs A list of HOC's to be applied using the parallel node hierarchy.  These will
+ *             be composed using lodash `flowRight`
+ *
+ * @return an HOC which can wrap any Component using the Bodiless `ContentNode` system.
+ */
 const withSidecarNodes = (...hocs: HOC[]) => flowRight(
-  withNode,
   startSidecarNodes,
   ...hocs,
   endSidecarNodes,
 );
 
 export default withSidecarNodes;
+export { startSidecarNodes, endSidecarNodes };
