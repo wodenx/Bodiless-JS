91d260d9ec0d76be31c3339d54b2ba4bde9275ed
Chris Oden <coden@its.jnj.com>
2020-08-21 18:41:37 -0400

    Not working - experiment

--- examples/test-site/src/data/pages/megamenu/MegaMenu.tsx
+++ examples/test-site/src/data/pages/megamenu/MegaMenu.tsx
@@ -12,14 +12,17 @@
  * limitations under the License.
  */
 
-import React, { ComponentType, PropsWithChildren, Fragment } from 'react';
-import { flow } from 'lodash';
-import { List } from '@bodiless/components';
+import React, { ComponentType, PropsWithChildren, Fragment, FC } from 'react';
+import { flow, identity } from 'lodash';
+import { List, Editable, asBodilessLink, withLinkToggle, asEditable } from '@bodiless/components';
+import ContentEditable from 'react-contenteditable';
+import { observer } from 'mobx-react-lite';
 import {
   asHorizontalMenu,
   asHorizontalSubMenu,
   withSubmenu,
   asMenuLink,
+  MenuLink,
 } from '@bodiless/organisms';
 
 import {
@@ -27,7 +30,7 @@ import {
 } from '@bodiless/fclasses';
 import { replaceWith } from '@bodiless/fclasses/src/Design';
 import { withTitle } from '@bodiless/layouts';
-import { useNode, NodeProvider } from '@bodiless/core';
+import { useNode, NodeProvider, withNode, withNodeKey, withChild } from '@bodiless/core';
 import { withEditorSimple } from '../../../components/Editors';
 import { asExceptMobile } from '../../../components/Elements.token';
 
@@ -35,7 +38,83 @@ import { withMenuListStyles, withMenuSublistStyles } from '../../../components/M
 import asChamelionTitle from './asChamelionTitle';
 import asBodilessChamelion from './Chamelion';
 import asMenuTout from './MenuTout';
-import asMenu, { asSubMenu } from './RCMenu';
+import asMenu, { asSubMenu, asMenuItemGroup } from './RCMenu';
+
+const asTitledItem = <P extends object>(Item: ComponentType<PropsWithChildren<P>>) => {
+  const TitledItem: ComponentType<P> = ({ children, ...rest }) => {
+    // prepare and pass the submenu title as a prop according to rc-menu <SubMenu /> specification
+    // wrap the title with current node,
+    // otherwise the title will read data from incorrect node when it is rendered by <SubMenu />
+    const { node } = useNode();
+    const children$ = <NodeProvider node={node}>{children}</NodeProvider>;
+    return (
+      <Item title={children$} {...rest as any} />
+    );
+  };
+  return TitledItem;
+};
+
+const FixedTitle = () => {
+  const { node } = useNode<{ text: string }>();
+  const onInput = (event: any) => {
+    const newText = event.currentTarget.innerHTML;
+    node.setData({ text: newText });
+  };
+  const text = node.data.text !== undefined ? node.data.text : '';
+  return (
+    <button
+      type="button"
+      onClick={e => {
+        e.stopPropagation();
+        e.preventDefault();
+      }}
+    >
+      <Editable />
+      {/*
+      <ContentEditable
+        tagName="span"
+        className="bodiless-inline-editable"
+        html={text}
+        onChange={onInput}
+        placeholder="Placeholder"
+      />
+      */}
+    </button>
+  );
+};
+
+const asFixedTitle = flow(
+  replaceWith(FixedTitle),
+  observer,
+  withNode,
+  withNodeKey('editable'),
+);
+
+const FixedLink = (props: any) => <a {...props}></a>;
+
+const asMenuLink$ = (editable: any) => flow(
+  replaceWith(FixedLink),
+  asBodilessLink('title-link'),
+  // withLinkToggle,
+  // withExtendHandler('onClick', () => stopPropagation),
+  // withNode, 
+  // withNodeKey('title'),
+  editable('text', 'Menu Item'),
+);
+
+const asGroup = flow(
+  replaceWith(List),
+  asMenuItemGroup,
+  asTitledItem,
+  withDesign({
+    Title: asMenuLink$(asEditable),
+    // Title: withChild(FixedSpan),
+    // Title: replaceWith(FixedLink),
+    // TItle: asFixedTitle,
+    // Title: asMenuLink$,
+  }),
+  // withMenuSublistStyles,
+);
 
 // Basic SubMemu
 const asBasicSubMenu = flow(
@@ -52,53 +131,36 @@ const asToutSubMenu = flow(
   asBasicSubMenu,
   withDesign({
     Title: asMenuTout,
+  }),
+  withDesign({
     Wrapper: addProps({ popupClassName: 'container bl-mega-menu' }),
     Item: addClasses('w-1/3'),
   }),
 );
 
-export const asTitledItem = <P extends object>(Item: ComponentType<PropsWithChildren<P>>) => {
-  const TitledItem: ComponentType<P> = ({ children, ...rest }) => {
-    // prepare and pass the submenu title as a prop according to rc-menu <SubMenu /> specification
-    // wrap the title with current node,
-    // otherwise the title will read data from incorrect node when it is rendered by <SubMenu />
-    const { node } = useNode();
-    const children$ = <NodeProvider node={node}>{children}</NodeProvider>;
-    return (
-      <Item title={children$} {...rest as any} />
-    );
-  };
-  return TitledItem;
-};
-
-const sublistChamelionDesign = {
-  Basic: flow(asBasicSubMenu, withTitle('Basic sub-menu')),
-  Touts: flow(asToutSubMenu, withTitle('Tout sub-menu')),
-};
+const asColumnSubMenu = flow(
+  replaceWith(List),
+  asSubMenu,
+  withDesign({
+    Title: asMenuLink(withEditorSimple),
+    Item: asGroup,
+  }),
+  withDesign({
+    Wrapper: addProps({ popupClassName: 'container bl-mega-menu' }),
+    Item: addClasses('w-1/3'),
+  }),
+  asHorizontalSubMenu,
+  withMenuSublistStyles,
+);
 
 const ChamelionSubMenu = flow(
   asBodilessChamelion('cham-sublist', { component: 'Basic' }),
-  withDesign(sublistChamelionDesign),
+  withDesign({
+    Basic: flow(asBasicSubMenu, withTitle('Basic sub-menu')),
+    Touts: flow(asToutSubMenu, withTitle('Tout sub-menu')),
+    Columns: flow(asColumnSubMenu, withTitle('Column sub-menu')),
+  }),
 )(Fragment);
-
-// const Group = flow(
-//   asMenuItemGroup,
-//   withDesign({
-//     Title: asMenuLink(withEditorSimple),
-//   }),
-//   withMenuSublistStyles,
-//   asTitledItem,
-// )(List);
-//
-// const ColumnSubMenu = flow(
-//   asSubMenu,
-//   withDesign({
-//     Title: asMenuLink(withEditorSimple),
-//     Item: replaceWith(Group),
-//   }),
-//   asHorizontalSubMenu,
-//   withMenuSublistStyles,
-// )(List);
 
 const Menu = flow(
   asMenu,
