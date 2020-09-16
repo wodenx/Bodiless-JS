68ec71dc527c0c728486fbb3f70f333431cba26d
Chris Oden <coden@its.jnj.com>
2020-09-16 12:13:10 -0400

    Complete renaming to list

--- examples/test-site/src/data/pages/list2/ListDemo.tsx
+++ examples/test-site/src/data/pages/list2/ListDemo.tsx
@@ -6,7 +6,7 @@ import {
   addClasses, withDesign, replaceWith, A,
 } from '@bodiless/fclasses';
 import {
-  asEditable, asSubList, withDeleteNodeOnUnwrap, asBodilessList, asChamelionSubList, asToggledSubList,
+  asEditable, asSubList, withDeleteNodeOnUnwrap, asBodilessList, asToggledSubList,
 } from '@bodiless/components';
 import { WithNodeKeyProps } from '@bodiless/core';
 import { ComponentType } from 'react';
@@ -72,38 +72,21 @@ const withListDesign = (design: any) => {
  *
  * @return HOC which creates a basic mega menu list.
  */
-const asMenuBase = (nodeKeys?: WithNodeKeyProps) => flow(
+const asListDemo = (nodeKeys?: WithNodeKeyProps) => flow(
   asBodilessList(nodeKeys),
   asStylableList,
   withDesign({
     Item: asToggledSubList,
   }),
   withSubListDesign(asDemoSubList),
-);
-
-/**
- * Copyright © 2020 Johnson & Johnson
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-const asMegaMenuBase$ = flow(
-  asMenuBase(),
-  withListDesign(withLinkTitle),
-);
-
-const MegaMenuList = flow(
-  asMegaMenuBase$,
-  withListDesign({
+  withSubListDesign({
     Item: addClasses('pl-5'),
   }),
+);
+
+const ListDemo = flow(
+  asListDemo(),
+  withListDesign(withLinkTitle),
 )('ul') as ComponentType<any>;
 
-export default MegaMenuList;
+export default ListDemo;
