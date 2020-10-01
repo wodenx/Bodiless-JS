c3adaa701d28637ec4684aa7fc0d250e7a488465
Chris Oden <coden@its.jnj.com>
2020-10-01 09:21:10 -0400

    Fix linting error

--- packages/bodiless-components/src/List/asBodilessList.tsx
+++ packages/bodiless-components/src/List/asBodilessList.tsx
@@ -111,7 +111,7 @@ const asSubList = flow(
 const withSimpleSubListDesign = (depth: number) => (withDesign$: HOC): HOC => (
   depth === 0 ? identity
     : withDesign({
-    Item: flow(
+      Item: flow(
         withDesign$,
         withSimpleSubListDesign(depth - 1)(withDesign$),
       ),
