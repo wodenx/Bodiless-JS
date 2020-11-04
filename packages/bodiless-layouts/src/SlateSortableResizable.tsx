6e12d43197bc17a8e09842c7da069d1e9247e187
Chris Oden <coden@its.jnj.com>
2020-11-03 19:06:40 -0500

    Remove unnecessary style prop on SlateSortableResizable

--- packages/bodiless-layouts/src/SlateSortableResizable.tsx
+++ packages/bodiless-layouts/src/SlateSortableResizable.tsx
@@ -50,7 +50,6 @@ type Props = {
     height?: string | number | undefined;
   };
   className: string;
-  style?: React.CSSProperties,
   useGetMenuOptions: () => TMenuOptionGetter;
   onResizeStop?: ResizeCallback;
   onResize?: ResizeCallback;
