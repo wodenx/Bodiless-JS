45ac5b29949749f679352d4caaaf8fb22c50609c
wodenx <coden@its.jnj.com>
2020-11-19 11:59:06 -0500

    Update index.tsx

--- packages/bodiless-layouts/src/ComponentSelector/index.tsx
+++ packages/bodiless-layouts/src/ComponentSelector/index.tsx
@@ -105,7 +105,7 @@ const ComponentSelector: React.FC<ComponentSelectorProps> = props => {
             <finalUI.ComponentLinkWrapper
               disabled={activeFilters.length === 0 && activeSearch.trim().length === 0}
               onClick={() => {
-                if (activeFilters.length !== 0 || activeSearch.length !== 0) {
+                if (activeFilters.length !== 0 || activeSearch.trim().length !== 0) {
                   setActiveSearch('');
                   setActiveFilters([]);
                 }
