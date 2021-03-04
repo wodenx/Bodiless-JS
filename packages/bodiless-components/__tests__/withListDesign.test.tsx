91ab295a94a041f45458c431038fbeebbb4d8f7e
Chris Oden <coden@its.jnj.com>
2021-03-04 16:22:38 -0500

    WIP on list design

--- packages/bodiless-components/__tests__/withListDesign.test.tsx
+++ packages/bodiless-components/__tests__/withListDesign.test.tsx
@@ -4,10 +4,14 @@ import {
   Token, withDesign, addProps, replaceWith, withoutProps,
 } from '@bodiless/fclasses';
 import { withDefaultContent } from '@bodiless/core';
-import { flow } from 'lodash';
+import { flow, isEqual } from 'lodash';
 import { render, ComponentType } from 'enzyme';
 import { withSubLists } from '../src/List/asChameleonSubList';
 import asBodilessList, { asSubList } from '../src/List/asBodilessList';
+
+import {
+  ListSchemaDef, buildTree, buildSchema, subKeysForPath, realPath,
+} from '../src/List/withListDesign';
 
 const withItemTitle = (title: string) => withDesign({
   Title: flow(
@@ -133,4 +137,65 @@ describe('list styling', () => {
     expect(wrapper.find('ul#top>li').prop('id')).toBe('title-sub-list');
     console.log(wrapper.html());
   });
+});
+
+
+
+describe('withListDesign', () => {
+  const schema: ListSchemaDef = {
+    keySpace: ['Bullets', 'Numbers', 'SubList'],
+    maxDepth: 3,
+    hasNode: path => {
+      if (!['_default', 'Bullets', 'Numbers'].includes(path[0])) return false;
+      if (path.length === 1) return true;
+      if (!['_default', 'SubList'].includes(path[1])) return false;
+      if (path.length === 2) return true;
+      if (path[1] === '_default') return false;
+      if (!['_default', 'SubList'].includes(path[2])) return false;
+      return true;
+    },
+  };
+
+  it('Test schema evaluates paths correctly', () => {
+    expect(schema.hasNode(['Bullets'])).toBeTruthy();
+    expect(schema.hasNode(['None'])).toBeTruthy();
+    expect(schema.hasNode(['Numbers'])).toBeTruthy();
+    expect(schema.hasNode(['Bullets', 'SubList'])).toBeTruthy();
+    expect(schema.hasNode(['Bullets', 'SubList', 'SubList'])).toBeTruthy();
+    expect(schema.hasNode(['Numbers', 'SubList', 'SubList'])).toBeTruthy();
+    expect(schema.hasNode(['Bullets', 'Numbers'])).toBeFalsy();
+    expect(schema.hasNode(['Numbers', 'Bullets'])).toBeFalsy();
+    expect(schema.hasNode(['Bullets', 'Numbers', 'SubList'])).toBeFalsy();
+    expect(schema.hasNode(['Numbers', 'Bullets', 'SubList'])).toBeFalsy();
+  });
+
+  it('Finds subkeys correctly', () => {
+    expect(subKeysForPath(schema)(['Bullets'])).toEqual(['_default', 'SubList']);
+    expect(subKeysForPath(schema)(['Bullets', '_default'])).toEqual([]);
+    expect(subKeysForPath(schema)(['Bullets', 'SubList'])).toEqual(['_default', 'SubList']);
+  });
+
+  it('Creates the correct paths list from a schema', () => {
+    const paths = buildSchema(schema);
+    const expected = [
+      [ '_default' ],
+      [ '_default', '_default' ],
+      [ '_default', 'SubList' ],
+      [ '_default', 'SubList', '_default' ],
+      [ '_default', 'SubList', 'SubList' ],
+      [ 'Bullets' ],
+      [ 'Bullets', '_default' ],
+      [ 'Bullets', 'SubList' ],
+      [ 'Bullets', 'SubList', '_default' ],
+      [ 'Bullets', 'SubList', 'SubList' ],
+      [ 'Numbers' ],
+      [ 'Numbers', '_default' ],
+      [ 'Numbers', 'SubList' ],
+      [ 'Numbers', 'SubList', '_default' ],
+      [ 'Numbers', 'SubList', 'SubList' ],
+    ];
+    expect(paths).toEqual(expected);
+  });
+
+  describe.only('realPath',  () => {
 });
--- packages/bodiless-components/src/List/withListDesign.ts
+++ packages/bodiless-components/src/List/withListDesign.ts
@@ -1,48 +1,37 @@
-import { identity, flow, set } from 'lodash';
+import { identity, flow, set, isEqual } from 'lodash';
 import { withDesign, HOC } from '@bodiless/fclasses';
 
-type Path = string[];
-type Schema = {
-  keySpace: Path,
+export type ListPath = string[];
+export type ListSchemaDef = {
+  keySpace: ListPath,
   maxDepth: number,
-  hasNode: (path: Path) => boolean,
+  hasNode: (path: ListPath) => boolean,
 };
 
 // Returns the options available at a given depth
-const subKeysForPath = (schema: Schema) => (path: Path) => (
-  path.length >= schema.maxDepth
-    ? [] : schema.keySpace.filter(key => schema.hasNode([...path, key]))
+export const subKeysForPath = (schema: ListSchema) => (path: ListPath) => (
 );
+
+
 
 // Translates a canonical list path to an actual list design path (inserts the
 // 'Item' key where necessary), based on the list schema.
-const realPath = (schema: Schema) => (pathIn: Path): Path => pathIn.reduce(
+export const realPath = (schema: ListSchema) => (pathIn: ListPath): ListPath => pathIn.reduce(
   (acc, key, depth) => {
     const parent = pathIn.slice(0, depth);
     const prefix = depth === 0 ? [] : ['Item'];
     const key$ = subKeysForPath(schema)(parent).length === 0 ? [] : [key];
     return [...acc, ...prefix, ...key$];
-  }, [] as Path,
+  }, [] as ListPath,
 );
 
 // Gives you all possible canonical paths for a given schema
 // @todo i think this does not properly limit to max-depth.
-const allCanonicalPaths = (schema: Schema, prefix: Path = []): Path[] => {
-  const keys = subKeysForPath(schema)(prefix);
-  return keys.reduce(
-    (acc, key) => [
-      ...acc,
-      [...prefix, key],
-      ...allCanonicalPaths(schema, [...prefix, key]),
-    ],
-    [] as Path[],
-  );
-};
 
 // Creates an object representing list structure from a schema definition.
-export const buildTree = (schema: Schema): any => {
-  const paths = allCanonicalPaths(schema);
-  const setPath = (path: Path) => (tree: any) => set(tree, path, true);
+export const buildTree = (schema: ListSchema): any => {
+  const paths = buildSchema(schema);
+  const setPath = (path: ListPath) => (tree: any) => set(tree, path, true);
   return flow(
     paths
       .sort((a, b) => a.length - b.length)
@@ -50,7 +39,7 @@ export const buildTree = (schema: Schema): any => {
   )({});
 };
 
-const withDesignAtSingle = (path:Path) => (hoc:HOC): HOC => {
+const withDesignAtSingle = (path:ListPath) => (hoc:HOC): HOC => {
   const [next, ...rest] = path;
   if (rest.length > 0) {
     return withDesign({
@@ -65,11 +54,11 @@ const withDesignAtSingle = (path:Path) => (hoc:HOC): HOC => {
   return identity as HOC;
 };
 
-const withDesignAt = (...paths:Path[]) => flow(...paths.map(withDesignAtSingle));
+const withDesignAt = (...paths:ListPath[]) => flow(...paths.map(withDesignAtSingle));
 
-const withListDesignAt = (schema: Schema) => (
-  (pathTest: (p: Path) => boolean) => withDesignAt(
-    ...allCanonicalPaths(schema)
+const withListDesignAt = (schema: ListSchema) => (
+  (pathTest: (p: ListPath) => boolean) => withDesignAt(
+    ...buildSchema(schema)
       .filter(pathTest)
       .map(realPath(schema)),
   )
@@ -78,7 +67,7 @@ const withListDesignAt = (schema: Schema) => (
 // ************************ EXAMPLES *********************************
 
 // Example schema for our default canvasx list
-const canvasxSchema: Schema = {
+const canvasxSchema: ListSchema = {
   keySpace: ['Bullets', 'Numbers', 'SubList'],
   maxDepth: 3,
   hasNode: path => path.reduce<boolean>(
@@ -92,9 +81,9 @@ const canvasxSchema: Schema = {
 };
 
 // canvasx path test to give us all numberted lists
-export const allNumbered = (path: Path) => path[0] === 'Numbered';
+export const allNumbered = (path: ListPath) => path[0] === 'Numbered';
 // canvasx path test to give us all lists at depth 2
-export const allDepth2 = (path: Path) => path.length === 2;
+export const allDepth2 = (path: ListPath) => path.length === 2;
 
 export const withCanvasXListDesign = (
   keys: string|string[] = ['_default', 'Bullets', 'Numbers'],
@@ -102,12 +91,12 @@ export const withCanvasXListDesign = (
 ) => {
   const keys$ = Array.isArray(keys) ? keys : [keys];
   const depths$ = Array.isArray(depths) ? depths : [depths];
-  const test = (path: Path) => keys$.includes(path[0]) && depths$.includes(path.length);
+  const test = (path: ListPath) => keys$.includes(path[0]) && depths$.includes(path.length);
   return withListDesignAt(canvasxSchema)(test);
 };
 
 // Example schema for megamenu
-const menuSchema: Schema = {
+const menuSchema: ListSchema = {
   keySpace: ['Columns', 'List', 'Touts', 'SubColumn'],
   maxDepth: 3,
   hasNode: path => {
@@ -127,10 +116,67 @@ export const withCanvasXMenuDesign = (
 ) => {
   const keys$ = Array.isArray(keys) ? keys : [keys];
   const depths$ = Array.isArray(depths) ? depths : [depths];
-  const test = (path: Path) => {
+  const test = (path: ListPath) => {
     if (!depths$.includes(path.length)) return false;
     if (path.length === 0) return keys$.includes(path[0]);
     return keys$.includes(path[1]);
   };
   return withListDesignAt(menuSchema)(test);
+};
+
+
+type ListSchema = {
+  allPaths: ListPath[],
+  hasPath: (path: ListPath) => boolean,
+  subPaths: (path: ListPath) => ListPath[],
+}
+
+class BasicListSchema implements ListSchema {
+  protected paths: ListPath[];
+
+  constructor(paths: ListPath[]) {
+    this.paths = paths;
+  }
+
+  get allPaths() {
+    return this.paths;
+  }
+
+  hasPath(path: ListPath) {
+    return this.paths.filter(p => isEqual(p, path)).length > 0;
+  }
+
+  subPaths(path: ListPath) {
+    const { length } = path;
+    return this.paths
+      .filter(p => isEqual(p.slice(0, length), path))
+      .filter(p => p.length > length)
+      .map(p => p.slice(length));
+  }
+
+  realPath(path: ListPath): ListPath {
+    return path.reduce(
+      (acc, key, depth) => {
+        const parent = path.slice(0, depth);
+        const prefix = depth === 0 ? [] : ['Item'];
+        const key$ = this.subPaths(parent).length === 0 ? [] : [key];
+        return [...acc, ...prefix, ...key$];
+      }, [] as ListPath,
+    );
+  }
+}
+
+
+export const listAllPaths = (def: ListSchemaDef, prefix: ListPath = []): ListPath[] => {
+  const keys = prefix.length >= def.maxDepth
+    // @todo Maybe we should always return at least _default for top level.
+    ? [] : [...['_default', ...def.keySpace].filter(key => def.hasNode([...def, key]))]
+  return keys.reduce(
+    (acc, key) => [
+      ...acc,
+      [...prefix, key],
+      ...listAllPaths(def, [...prefix, key]),
+    ],
+    [] as ListPath[],
+  );
 };
